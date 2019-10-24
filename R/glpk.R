39d278be9d3a2676827c0d3975714704da3256c9
Dirk Schumacher <mail@dirk-schumacher.net>
2019-10-24 21:40:03 +0200

    Fix a bug in the validation part

--- R/glpk.R
+++ R/glpk.R
@@ -12,7 +12,7 @@ m <- 1000
 k <- 10
 v <- matrix(round(runif(m * k) * 100), nrow = m, ncol = k)
 c <- matrix(round(runif(m * k) * 100), nrow = m, ncol = k)
-C <- sum(c) / 2
+C <- sum(c) / (2 * k)
 
 # some riders do not have the full k incentives,
 # but only the first k_i
@@ -45,47 +45,54 @@ for (rider in 1:m) {
 system.time(model$optimize())
 #> <SOLVER MSG>  ----
 #> GLPK Simplex Optimizer, v4.65
-#> 1001 rows, 10000 columns, 19950 non-zeros
+#> 1001 rows, 10000 columns, 19949 non-zeros
 #> Preprocessing...
-#> 852 rows, 4877 columns, 4877 non-zeros
+#> 851 rows, 4996 columns, 9824 non-zeros
 #> Scaling...
-#>  A: min|aij| =  1.000e+00  max|aij| =  1.000e+00  ratio =  1.000e+00
-#> Problem data seem to be well scaled
+#>  A: min|aij| =  1.000e+00  max|aij| =  1.000e+02  ratio =  1.000e+02
+#> GM: min|aij| =  3.162e-01  max|aij| =  3.162e+00  ratio =  1.000e+01
+#> EQ: min|aij| =  1.000e-01  max|aij| =  1.000e+00  ratio =  1.000e+01
 #> Constructing initial basis...
-#> Size of triangular part is 852
-#> *     0: obj =   7.284000000e+03 inf =   0.000e+00 (4856)
-#> Perturbing LP to avoid stalling [734]...
-#> Removing LP perturbation [1702]...
-#> *  1702: obj =   7.783400000e+04 inf =   0.000e+00 (0)
+#> Size of triangular part is 851
+#> *     0: obj =  -0.000000000e+00 inf =   0.000e+00 (4977)
+#> Perturbing LP to avoid stalling [604]...
+#> Removing LP perturbation [1894]...
+#> *  1894: obj =   6.876793481e+04 inf =   5.493e-14 (0)
 #> OPTIMAL LP SOLUTION FOUND
 #> GLPK Integer Optimizer, v4.65
-#> 1001 rows, 10000 columns, 19950 non-zeros
-#> 10000 integer variables, 5025 of which are binary
+#> 1001 rows, 10000 columns, 19949 non-zeros
+#> 10000 integer variables, 4996 of which are binary
 #> Preprocessing...
-#> 852 rows, 4877 columns, 4877 non-zeros
-#> 4877 integer variables, all of which are binary
+#> 851 rows, 4996 columns, 9824 non-zeros
+#> 4996 integer variables, all of which are binary
 #> Scaling...
-#>  A: min|aij| =  1.000e+00  max|aij| =  1.000e+00  ratio =  1.000e+00
-#> Problem data seem to be well scaled
+#>  A: min|aij| =  1.000e+00  max|aij| =  1.000e+02  ratio =  1.000e+02
+#> GM: min|aij| =  3.162e-01  max|aij| =  3.162e+00  ratio =  1.000e+01
+#> EQ: min|aij| =  1.000e-01  max|aij| =  1.000e+00  ratio =  1.000e+01
+#> 2N: min|aij| =  6.250e-02  max|aij| =  1.312e+00  ratio =  2.100e+01
 #> Constructing initial basis...
-#> Size of triangular part is 852
+#> Size of triangular part is 851
 #> Solving LP relaxation...
 #> GLPK Simplex Optimizer, v4.65
-#> 852 rows, 4877 columns, 4877 non-zeros
-#> *  1702: obj =   7.284000000e+03 inf =   0.000e+00 (4856)
-#> Perturbing LP to avoid stalling [2436]...
-#> Removing LP perturbation [3404]...
-#> *  3404: obj =   7.783400000e+04 inf =   0.000e+00 (0)
+#> 851 rows, 4996 columns, 9824 non-zeros
+#> *  1894: obj =  -0.000000000e+00 inf =   0.000e+00 (4977)
+#> Perturbing LP to avoid stalling [2450]...
+#> Removing LP perturbation [3896]...
+#> *  3896: obj =   6.876793481e+04 inf =   1.459e-13 (0)
 #> OPTIMAL LP SOLUTION FOUND
 #> Integer optimization begins...
 #> Long-step dual simplex will be used
-#> +  3404: mip =     not found yet <=              +inf        (1; 0)
-#> +  3404: >>>>>   7.783400000e+04 <=   7.783400000e+04   0.0% (1; 0)
-#> +  3404: mip =   7.783400000e+04 <=     tree is empty   0.0% (0; 1)
+#> +  3896: mip =     not found yet <=              +inf        (1; 0)
+#> Solution found by heuristic: 68753
+#> Solution found by heuristic: 68759
+#> Solution found by heuristic: 68765
+#> Solution found by heuristic: 68766
+#> Solution found by heuristic: 68767
+#> +  4580: mip =   6.876700000e+04 <=     tree is empty   0.0% (0; 317)
 #> INTEGER OPTIMAL SOLUTION FOUND
 #> <!SOLVER MSG> ----
 #>    user  system elapsed
-#>   0.229   0.005   0.234
+#>   1.408   0.015   1.450
 
 # get all variables values as a data frame and retain only those that
 # have a value of 1
@@ -99,32 +106,34 @@ result %>%
 #> # Groups:   j [10]
 #>        j     n
 #>    <int> <int>
-#>  1     1   348
-#>  2     2   208
+#>  1     1   254
+#>  2     2   169
 #>  3     3   150
-#>  4     4    83
-#>  5     5    77
-#>  6     6    52
-#>  7     7    30
-#>  8     8    35
-#>  9     9    12
-#> 10    10     5
+#>  4     4   102
+#>  5     5    81
+#>  6     6    66
+#>  7     7    36
+#>  8     8    24
+#>  9     9    14
+#> 10    10     7
 
 # it is also a good idea to check that the resulting data satisfies the constraints
 
 # check that the cost is within the budget
+cols_with_vals <- sort(unique(result$j))
+rows_with_vals <- sort(unique(result$i))
 assignment_matrix <- tidyr::spread(result, "j", "value", fill = 0) %>%
   select(-name, -i) %>%
   as.matrix()
 
-(cost <- sum(assignment_matrix * c))
-#> [1] 50264
+(cost <- sum(assignment_matrix * c[rows_with_vals, cols_with_vals]))
+#> [1] 24763
 cost <= C
 #> [1] TRUE
 
 # total value is
-(value <- sum(assignment_matrix * v))
-#> [1] 77834
+(value <- sum(assignment_matrix * v[rows_with_vals, cols_with_vals]))
+#> [1] 68767
 
 # as a last check we verify that a rider only has one incentive at most
 all(rowSums(assignment_matrix) <= 1)
